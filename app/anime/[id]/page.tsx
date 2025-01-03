08c4e75e4b06b2dda012d201cfd089d1101cc296
SaukiFutaki <utaki.sauki19@gmail.com>
2025-01-03 16:49:01 +0700

    detail page

--- app/anime/[id]/page.tsx
+++ app/anime/[id]/page.tsx
@@ -43,7 +43,7 @@ async function fetchAnimeData(id: string) {
 }
 
 export default async function Page({ params }: PageProps) {
-  if (!(await params)?.id) return notFound();
+  // if (!(await params)?.id) return notFound();
 
   const { anime: data, recommendations } = await fetchAnimeData((await params).id);
 
