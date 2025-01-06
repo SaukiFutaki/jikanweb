78bc7843de17afdcf13e25dd6f285bca814a41e1
SaukiFutaki <utaki.sauki19@gmail.com>
2025-01-06 09:59:26 +0700

    d

--- app/detail/anime/[id]/page.tsx
+++ app/detail/anime/[id]/page.tsx
@@ -2,7 +2,7 @@ import { getAnimeById } from '@/lib/actions/anime';
 import React from 'react'
 
 interface PageProps {
-  params: Promise<{ id: string }>;
+  params: Promise<{ id: number }>;
 }
 
 export default async function Page({params} : PageProps) {
